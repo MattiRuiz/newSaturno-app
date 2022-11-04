a6e368ff6884db4e4e0ff58d87497048c6541dcd
Matias Ruiz <matias.ruiz.dg@gmail.com>
2022-11-04 15:57:36 -0300

    Solucionado errores de composición

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-import { Button, Container } from "reactstrap";
-import "./SignUp.css";
 
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
@@ -31,121 +29,28 @@ const SignUp = () => {
     setUserName(e.target.value);
   };
 
-  const userNameValidation = () => {
-    if (userName === "") {
-      setErrors({ ...errors, userName: "Campo obligatorio." });
-    } else if (userName.length < 4 || userName.length > 10) {
-      setErrors({
-        ...errors,
-        userName: "Debe contener entre 4 y 10 caracteres.",
-      });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.userName;
-      setErrors(_errors);
-    }
-  };
-
   const nameLastnameHandler = (e) => {
     setNameLastname(e.target.value);
-  };
-
-  const nameLastnameValidation = () => {
-    if (nameLastname === "") {
-      setErrors({ ...errors, nameLastname: "Campo obligatorio." });
-    } else if (nameLastname.length < 5 || nameLastname.length > 25) {
-      setErrors({
-        ...errors,
-        nameLastname: "Debe contener entre 5 y 25 caracteres.",
-      });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.nameLastname;
-      setErrors(_errors);
-    }
   };
 
   const emailHandler = (e) => {
     setEmail(e.target.value);
   };
 
-  const emailValidation = () => {
-    const validEmail = "@";
-    const correct = email.match(validEmail);
-    if (email === "") {
-      setErrors({ ...errors, email: "Campo obligatorio." });
-    } else if (!correct) {
-      setErrors({ ...errors, email: "Ingrese un email correcto." });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.email;
-      setErrors(_errors);
-    }
-  };
-
   const phoneNumberHandler = (e) => {
     setPhoneNumber(e.target.value);
-  };
-
-  const phoneNumberValidation = () => {
-    if (phoneNumber === "") {
-      setErrors({ ...errors, phoneNumber: "Campo obligatorio." });
-    } else if (phoneNumber.length < 10 || phoneNumber.length > 10) {
-      setErrors({ ...errors, phoneNumber: "Debe contener 10 números." });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.phoneNumber;
-      setErrors(_errors);
-    }
   };
 
   const ubicationHandler = (e) => {
     setUbication(e.target.value);
   };
 
-  const ubicationValidation = () => {
-    if (ubication === "") {
-      setErrors({ ...errors, ubication: "Campo obligatorio." });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.ubication;
-      setErrors(_errors);
-    }
-  };
-
   const passwordHandler = (e) => {
     setPassword(e.target.value);
   };
 
-  const passwordValidation = () => {
-    if (password === "") {
-      setErrors({ ...errors, password: "Campo obligatorio." });
-    } else if (password.length < 5 || password.length > 10) {
-      setErrors({
-        ...errors,
-        password: "Debe contener entre 5 y 10 caracteres.",
-      });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.password;
-      setErrors(_errors);
-    }
-  };
-
   const validPasswordHandler = (e) => {
     setValidPassword(e.target.value);
-  };
-
-  const validPasswordValidation = () => {
-    if (validPassword === "") {
-      setErrors({ ...errors, validPassword: "Campo obligatorio." });
-    } else if (validPassword !== password) {
-      setErrors({ ...errors, validPassword: "Las contraseñas no coinciden." });
-    } else {
-      let _errors = { ...errors };
-      delete _errors.validPassword;
-      setErrors(_errors);
-    }
   };
 
   const saveBaseUsuarioHandler = () => {
@@ -157,47 +62,9 @@ const SignUp = () => {
       ubication: ubication,
       password: password,
     };
-
-    if (
-      Object.keys(errors).length === 0 &&
-      userName &&
-      nameLastname &&
-      email &&
-      phoneNumber &&
-      ubication &&
-      password &&
-      validPassword
-    ) {
-      setUserData([usuarioDatos, ...baseUsuarios]);
-      cleanInputs();
-      window.localStorage.setItem("baseUsuario", JSON.stringify(userData));
-      console.log(userData);
-      setErrorsValidation("");
-    } else {
-      if (errors.userName) {
-        inputUserName.current.focus();
-      }
-      if (errors.nameLastname) {
-        inputNameLast.current.focus();
-      }
-      if (errors.email) {
-        inputEmail.current.focus();
-      }
-      if (errors.phoneNumber) {
-        inputPhoneNumber.current.focus();
-      }
-      if (errors.ubication) {
-        inputUbication.current.focus();
-      }
-      if (errors.password) {
-        inputPassword.current.focus();
-      }
-      if (errors.validPassword) {
-        inputValidPassword.current.focus();
-      }
-
-      setErrorsValidation("Por favor ingrese los datos correctamente.");
-    }
+    setUserData([usuarioDatos, ...baseUsuarios]);
+    window.localStorage.setItem("baseUsuario", JSON.stringify(userData));
+    console.log(userData);
   };
 
   const cleanInputs = () => {
@@ -211,55 +78,92 @@ const SignUp = () => {
   };
 
   return (
-    <Container className="text-center py-5">
-      <h2>Registrarse</h2>
-      <form className="signUpForm">
-        <label>Nombre de usuario: </label>
-        <input onChange={userNameHandler} value={userName} type="text"></input>
-        <label>Nombre y apellido: </label>
-        <input
-          onChange={nameLastnameHandler}
-          value={nameLastname}
-          type="text"
-        ></input>
-        <label>Email: </label>
-        <input onChange={emailHandler} value={email} type="email"></input>
-        <label>Número de celular: </label>
-        <input
-          onChange={phoneNumberHandler}
-          value={phoneNumber}
-          type="text"
-        ></input>
-        <label>Ubicación:</label>
-        <select onChange={ubicationHandler} value={ubication}>
-          <option value="Rosario">Rosario</option>
-          <option value="Arroyo Seco">Arroyo Seco</option>
-          <option value="VGG">Villa Gobernador Galvez</option>
-          <option value="Baigorria">Granadero Baigorria</option>
-        </select>
-        <label>Contraseña: </label>
-        <input
-          onChange={passwordHandler}
-          value={password}
-          type="password"
-        ></input>
-        <label>Repita la contraseña: </label>
-        <input
-          onChange={validPasswordHandler}
-          value={validPassword}
-          type="password"
-        ></input>
-      </form>
-      <div className="buttonForm">
-        <Button
-          onClick={saveBaseUsuarioHandler}
-          className="botonEnviar"
-          color="primary"
-        >
-          Enviar
-        </Button>
-        <Button onClick={cleanInputs}>Limpiar</Button>
-      </div>
+    <Container className="py-3">
+      <Row className="justify-content-center text-start">
+        <Col xs={12} lg={10} xl={7} className="border-bottom pb-4 mb-4">
+          <h1>Registrarse</h1>
+        </Col>
+        <Col xs={12} lg={10} xl={7} className="mt-2">
+          <Form>
+            <Form.Group>
+              <Form.Label>Nombre de usuario:</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={userNameHandler}
+                value={userName}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4">
+              <Form.Label>Nombre y apellido:</Form.Label>
+              <Form.Control
+                type="text"
+                onChange={nameLastnameHandler}
+                value={nameLastname}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4">
+              <Form.Label>E-mail:</Form.Label>
+              <Form.Control
+                type="email"
+                onChange={emailHandler}
+                value={email}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4">
+              <Form.Label>Número de celular:</Form.Label>
+              <Form.Control
+                type="tel"
+                onChange={phoneNumberHandler}
+                value={phoneNumber}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4">
+              <Form.Label>Ubicación:</Form.Label>
+              <Form.Select
+                aria-label="select your city"
+                onChange={ubicationHandler}
+                value={ubication}
+              >
+                <option>Elija su ciudad</option>
+                <option value="Rosario">Rosario</option>
+                <option value="Arroyo Seco">Arroyo Seco</option>
+                <option value="VGG">Villa Gobernador Galvez</option>
+                <option value="Baigorria">Baigorria</option>
+              </Form.Select>
+            </Form.Group>
+            <Form.Group className="mt-4">
+              <Form.Label>Contraseña:</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={passwordHandler}
+                value={password}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4">
+              <Form.Label>Repita la contraseña:</Form.Label>
+              <Form.Control
+                type="password"
+                onChange={validPasswordHandler}
+                value={validPassword}
+              />
+            </Form.Group>
+            <Button
+              variant="primary"
+              className="mt-4"
+              onClick={saveBaseUsuarioHandler}
+            >
+              Enviar
+            </Button>
+            <Button
+              variant="secondary"
+              className="mt-4 mx-2"
+              onClick={cleanInputs}
+            >
+              Resetear
+            </Button>
+          </Form>
+        </Col>
+      </Row>
     </Container>
   );
 };
