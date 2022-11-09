72c69f2cc4496ed1787a17d29aec0de467ceb31f
sofiacorvalan <corvalansofia1@gmail.com>
2022-11-09 20:55:56 -0300

    corregida imagen en CardProfesional

--- src/Components/Home/CardsProfesionales/CardProfesional.js
+++ src/Components/Home/CardsProfesionales/CardProfesional.js
@@ -1,17 +1,19 @@
 import React from "react";
-
+import { useContext }from "react";
 import "./CardProfesional.css";
 
 import Card from "react-bootstrap/Card";
 import { FiClock } from "react-icons/fi";
 import { BiMap } from "react-icons/bi";
+import ThemeContext from "../../../Contexts/ThemeContext/ThemeContext";
 
 const CardProfesional = () => {
   const cardImg = require("./smart-fit.jpg");
+  const {theme, handleTheme} = useContext(ThemeContext);
   return (
     <>
       <Card>
-        <Card.Img variant="top" src={cardImg} />
+        <Card.Img className={theme} variant="top" src={cardImg} />
         <Card.Body>
           <Card.Title>Smart Fit</Card.Title>
           <Card.Subtitle className="text-muted mb-2">Gimnasio</Card.Subtitle>
